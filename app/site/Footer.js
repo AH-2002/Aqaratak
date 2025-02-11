985d58f8e0ad8310f66873768ce5e37aa2d42992
AbdoH-2002 <a_hamada@b-circles.co>
2025-02-11 16:08:51 +0200

    Update Footer.js

--- app/site/Footer.js
+++ app/site/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <section style={{ padding: '20px 0', color: 'white', background: 'linear-gradient(to bottom, rgba(75, 2, 75, 0.655), rgba(213, 56, 213, 0.852))' }}>
             <div style={{ width: '90%', margin: 'auto' }}>
-                <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+               <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
                     <div>
                         <div>
                             <Image src="/logo.png" alt="Logo" width={70} height={70} />
@@ -18,63 +18,65 @@ export default function Footer() {
 
                     </div>
                     <div>
+                        <div>
 
-                        <h3 style={{ fontSize: 'larger', fontWeight: 'bolder' }}>
-                            Project
-                        </h3>
-                        <ul style={{ listStyle: "none", display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                            <h3 style={{ fontSize: 'larger', fontWeight: 'bolder' }}>
+                                Project
+                            </h3>
+                            <ul style={{ listStyle: "none", display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
 
-                            <li style={{ margin: '10px 0' }}>
-                                Download
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                Changing
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                Commision Icons
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                All Versions
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3 style={{ fontSize: 'larger', fontWeight: 'bolder' }}>
-                            Community
-                        </h3>
-                        <ul style={{ listStyle: "none", display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                            <li style={{ margin: '10px 0' }}>
-                                Github
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                Icon Requests
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                Twitter
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                Blog Awesome
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3 style={{ fontSize: 'larger', fontWeight: 'bolder' }}>
-                            Help
-                        </h3>
-                        <ul style={{ listStyle: "none", display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                            <li style={{ margin: '10px 0' }}>
-                                Support
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                Troubleshooting
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                Contact Us
-                            </li>
-                            <li style={{ margin: '10px 0' }}>
-                                Status
-                            </li>
-                        </ul>
+                                <li style={{ margin: '10px 0' }}>
+                                    Download
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    Changing
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    Commision Icons
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    All Versions
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3 style={{ fontSize: 'larger', fontWeight: 'bolder' }}>
+                                Community
+                            </h3>
+                            <ul style={{ listStyle: "none", display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                                <li style={{ margin: '10px 0' }}>
+                                    Github
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    Icon Requests
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    Twitter
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    Blog Awesome
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3 style={{ fontSize: 'larger', fontWeight: 'bolder' }}>
+                                Help
+                            </h3>
+                            <ul style={{ listStyle: "none", display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                                <li style={{ margin: '10px 0' }}>
+                                    Support
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    Troubleshooting
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    Contact Us
+                                </li>
+                                <li style={{ margin: '10px 0' }}>
+                                    Status
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '40px' }}>
