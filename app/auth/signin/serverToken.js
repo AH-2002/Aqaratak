9100307e796b8f27f68d1c6d8b5a6b1dc86fa877
AbdoH-2002 <a_hamada@b-circles.co>
2025-03-21 00:11:17 +0200

    Update serverToken.js

--- app/auth/signin/serverToken.js
+++ app/auth/signin/serverToken.js
@@ -4,7 +4,6 @@ import { cookies } from "next/headers";
 export async function setUserToken(token) {
     cookies().set("userToken", token, {
         httpOnly: true, // Secure server-only access
-        secure: process.env.NODE_ENV === "production", // Enforce security in production
         maxAge: 7 * 24 * 60 * 60, // Expire in 7 days
         path: "/", // Accessible everywhere in the app
     });
