7ac84b6f67fb1aa95187f928627416331825a210
AbdoH-2002 <a_hamada@b-circles.co>
2025-03-21 02:09:26 +0200

    Update UpdatePropertyButton.js

--- app/site/RentPage/UpdatePropertyButton.js
+++ app/site/RentPage/UpdatePropertyButton.js
@@ -58,7 +58,7 @@ export default function UpdatePropertyButton({ property, refreshProperties }) {
         setError(null);
 
         const api_URL = "https://realestate.learnock.com/";
-        const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+        const apiKey = 1234;
 
         try {
             const formDataToSend = new FormData();
