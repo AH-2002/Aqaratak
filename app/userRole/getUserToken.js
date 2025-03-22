ee2c38accb87032d0eeb5e4fc467d6235729b320
AbdoH-2002 <a_hamada@b-circles.co>
2025-03-22 02:58:02 +0200

    Delete app/userRole directory

--- app/userRole/getUserToken.js
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { cookies } from "next/headers";
-
-export async function getUserToken() {
-    const cookieStore = cookies();
-    return cookieStore.get("userToken")?.value || null;
-}
